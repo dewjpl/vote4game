abdd3767b9b8a38462fd54e6b1f8d98a94e13e93
Oskar Pietrzak <u114_oskpie_waw@technischools.com>
2024-06-12 14:09:13 +0200

    gg

--- my-gaming-vote-app/pages/index.tsx
+++ my-gaming-vote-app/pages/index.tsx
@@ -74,7 +74,7 @@ const Home: React.FC = () => {
 	return (
 		<div className="container mx-auto px-4">
 			<h1 className="text-3xl font-bold text-center my-10 text-purple-400">
-				Zagłosuj na swoją ulubionę grę.
+				Zagłosuj na swoją ulubioną grę.
 			</h1>
 			{voting && (
 				<div className="text-center mb-4 font-bold">
